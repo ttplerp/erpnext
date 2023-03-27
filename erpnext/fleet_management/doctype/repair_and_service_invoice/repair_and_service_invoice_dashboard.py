302a24df1e6c91161730ca5ab923f0822a2c59bf
=Biren <>
2023-03-27 15:10:56 +0600

    sales progress report changes applied

--- erpnext/fleet_management/doctype/repair_and_service_invoice/repair_and_service_invoice_dashboard.py
+++ erpnext/fleet_management/doctype/repair_and_service_invoice/repair_and_service_invoice_dashboard.py
@@ -3,7 +3,10 @@ from frappe import _
 def get_data():
 	return {
         "fieldname": "reference_name",
+		"non_standard_fieldnames": {
+			"Repair And Service": "repair_and_services",
+		},
 		"transactions": [
-			{"label": _("Related Transaction"), "items": ["Payment Entry"]},
+			{"label": _("Related Transaction"), "items": ["Payment Entry","Repair And Service"]},
 		],
 	}
