eb2f940c8d307d13da89131f5aaf58e0fe23dcca
jai <jai>
2023-04-07 16:17:41 +0600

    consolidated budget report dsp

---
+++ erpnext/budget/report/consolidated_budget_report_dsp/consolidated_budget_report_dsp.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _, _dict
+from frappe.utils import cstr, getdate, flt
+
+def execute(filters=None):
+	columns = get_columns(filters)
+	data = get_data(filters)
+
+	return columns, data
+
+def get_data(filters):
+	condition = ""
+	if filters.cost_center and not filters.group_by_account:
+		lft, rgt = frappe.db.get_value("Cost Center", filters.cost_center, ["lft", "rgt"])
+		condition += """ and (b.cost_center in (select a.name 
+											from `tabCost Center` a 
+											where a.lft >= {1} and a.rgt <= {2} and center_category = 'Domain'
+											) 
+					 or b.cost_center = '{0}')
+				""".format(filters.cost_center, lft, rgt)
+	
+	datas = frappe.db.sql("""select b.cost_center, b.project,
+			SUM(ba.budget_amount) as budget_amount, 
+			SUM(ba.initial_budget) as initial_budget, 
+			SUM(ba.budget_received) as added, 
+			SUM(ba.budget_sent) as deducted, 
+			SUM(ba.supplementary_budget) as supplement
+		from `tabBudget` b, `tabBudget Account` ba 
+		where b.docstatus = 1 
+			and b.name = ba.parent 
+			and b.fiscal_year = '{fiscal_year}'
+		{condition}
+		group by b.cost_center
+		""".format(fiscal_year=filters.fiscal_year, condition=condition), as_dict=True)
+
+	final_data = []
+	ini = su = cur = cm = co = ad = av = 0
+	cond=""
+	for d in datas:
+		lft, rgt = frappe.db.get_value("Cost Center", d.cost_center, ["lft", "rgt"])
+		cond += """ and (cost_center in (select a.name 
+											from `tabCost Center` a 
+											where a.lft >= {1} and a.rgt <= {2}
+											) 
+					 or cost_center = '{0}')
+				""".format(d.cost_center, lft, rgt)
+
+		committed = frappe.db.sql("select SUM(amount) from `tabCommitted Budget` where reference_date BETWEEN '{0}' and '{1}' {cond}".format(filters.from_date, filters.to_date, cond=cond))[0][0]
+		consumed = frappe.db.sql("select SUM(amount) from `tabConsumed Budget` where reference_date BETWEEN '{0}' and '{1}' {cond}".format(filters.from_date, filters.to_date, cond=cond))[0][0]
+
+		if not committed:
+			committed = 0
+		if not consumed:
+			consumed = 0
+			
+		adjustment = flt(d.added) - flt(d.deducted)
+		supplement = flt(d.supplement)
+		# current    = flt(d.initial_budget) + flt(d.supplement) +flt(adjustment)
+		
+		if committed > 0:
+			committed -= consumed
+			committed = 0 if committed < 0 else committed
+
+		available = flt(d.initial_budget) + flt(adjustment) + flt(d.supplement) - flt(consumed) - flt(committed)
+		if d.budget_amount > 0:
+			row = {
+				"cost_center": d.cost_center,
+				"initial": flt(d.initial_budget),
+				"supplementary": supplement,
+				"adjustment": adjustment,
+				# "current": current,
+				"committed": committed,
+				"consumed": consumed,
+				"available": available
+			}
+
+			final_data.append(row)
+			ini+=flt(d.initial_budget)
+			su+=supplement
+			cm+=committed
+			# cur+=current
+			co+=consumed
+			ad+=adjustment
+			av+=available
+	row = {
+		"cost_center": "Total",
+		"initial": ini,
+		"supplementary": su,
+		"adjustment": ad,
+		"committed": cm,
+		"consumed": co,
+		"available": av
+	}
+	final_data.append(row)
+	return final_data
+		
+def get_columns(filters):
+	return [
+		{
+			"label": _("Domain"),
+			"fieldname": "cost_center",
+			"fieldtype": "Link",
+			"options": "Cost Center",
+			"width": 120,
+		},
+		{
+			"label": _("Initial"),
+			"fieldname": "initial",
+			"fieldtype": "Data",
+			"width": 120,
+		},
+		{
+			"label": _("Supplement"),
+			"fieldname": "supplement",
+			"fieldtype": "Data",
+			"width": 120,
+		},
+		{
+			"label": _("Adjustment"),
+			"fieldname": "adjustment",
+			"fieldtype": "Data",
+			"width": 120,
+		},
+		{
+			"label": _("Committed"),
+			"fieldname": "committed",
+			"fieldtype": "Data",
+			"width": 120,
+		},
+		{
+			"label": _("Consumed"),
+			"fieldname": "consumed",
+			"fieldtype": "Data",
+			"width": 120,
+		},
+		{
+			"label": _("Available"),
+			"fieldname": "available",
+			"fieldtype": "Data",
+			"width": 120,
+		}
+	]
