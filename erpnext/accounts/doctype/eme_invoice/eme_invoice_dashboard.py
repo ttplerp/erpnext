1110de73a2c881a44d726b7b5afcf09a9b36ff4e
birendra-bit <bhujelbirendra3@gmail.com>
2023-04-19 13:18:25 +0600

    stock aging report changes made

--- erpnext/accounts/doctype/eme_invoice/eme_invoice_dashboard.py
+++ erpnext/accounts/doctype/eme_invoice/eme_invoice_dashboard.py
@@ -3,9 +3,6 @@ from frappe import _
 def get_data():
 	return {
         "fieldname": "reference_name",
-		"non_standard_fieldnames": {
-			"Journal Entry": "referece_doctype"
-		},
 		"transactions": [
 			{"label": _("Related Transaction"), "items": ["Journal Entry"]},
 		],
