85aa33b78d57246dd01ddcfa9d2f9e715c89dd19
jai <jai>
2023-05-10 11:39:06 +0600

    tds remittance py

--- erpnext/accounts/doctype/tds_remittance/tds_remittance.py
+++ erpnext/accounts/doctype/tds_remittance/tds_remittance.py
@@ -18,6 +18,7 @@ class TDSRemittance(AccountsController):
 		self.make_gl_entries()
 
 	def on_cancel(self):
+		self.ignore_linked_doctypes = ("GL Entry", "Stock Ledger Entry", "Payment Ledger Entry")
 		self.make_gl_entries()
 	
 	@frappe.whitelist()
