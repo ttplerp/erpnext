feaab9a72b4beceaa891a3c34d6ff4a6fb354b23
birendra-bit <bhujelbirendra3@gmail.com>
2023-05-17 14:56:51 +0600

    journal entry display in asset dashboard done

--- erpnext/assets/doctype/asset/asset_dashboard.py
+++ erpnext/assets/doctype/asset/asset_dashboard.py
@@ -3,6 +3,7 @@ from frappe import _
 
 def get_data():
 	return {
+		"fieldname": "reference_name",
 		"non_standard_fieldnames": {"Asset Movement": "asset"},
-		"transactions": [{"label": _("Movement"), "items": ["Asset Movement"]}],
+		"transactions": [{"label": _("Movement"), "items": ["Asset Movement"], "items": ["Journal Entry"]}],
 	}
