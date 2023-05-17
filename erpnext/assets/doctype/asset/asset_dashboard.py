a8195d0350ef73daef8a3425fbacb5aa3f887aa6
birendra-bit <bhujelbirendra3@gmail.com>
2023-05-17 15:07:10 +0600

    journal entry display in asset dashboard done

--- erpnext/assets/doctype/asset/asset_dashboard.py
+++ erpnext/assets/doctype/asset/asset_dashboard.py
@@ -5,5 +5,5 @@ def get_data():
 	return {
 		"fieldname": "reference_name",
 		"non_standard_fieldnames": {"Asset Movement": "asset"},
-		"transactions": [{"label": _("Movement"), "items": ["Asset Movement"], "items": ["Journal Entry"]}],
+		"transactions": [{"label": _("Movement"), "items": ["Asset Movement"]},{"label":_("Journal Entry"), "items":["Journal Entry"]}],
 	}
