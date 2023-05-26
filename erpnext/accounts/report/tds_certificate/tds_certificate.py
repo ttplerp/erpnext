08785db9716a1c80b0fa41eafa98d6cde8c8bc28
kinleydorji <kd77144@gmail.com>
2023-05-26 12:54:46 +0600

    tds certificate

--- erpnext/accounts/report/tds_certificate/tds_certificate.py
+++ erpnext/accounts/report/tds_certificate/tds_certificate.py
@@ -5,20 +5,58 @@ import frappe
 from frappe import _
 from frappe.utils import flt
 
-# changes made by dorji 05/07/22 for issue #1911
-# added line 91, added cases from line 93-103, added line 112, added cases from line 114-123, added line 136
-
 def execute(filters=None):
-	currency_symbol=''
-	if filters.currency:
-		currency_symbol= frappe.db.sql("""select symbol from `tabCurrency` where name=%s""",(filters.currency))[0][0]
-	columns = get_columns(currency_symbol)
-
-	data = get_datax(filters)
-	data.append(calculate_total(data))
+	columns = get_columns()
+	data = get_data(filters)
 	return columns, data
-	
-def get_columns(currency_symbol):
+def get_data(filters):
+	data = []
+	conditions = get_condition(filters)
+	for d in frappe.db.sql('''
+			SELECT ti.party, ti.posting_date, ti.bill_amount, t.tax_withholding_category, 
+				ti.tds_amount, t.cheque_no, t.cheque_date, ti.invoice_no, ti.invoice_type,
+				t.tds_receipt_number, t.tds_receipt_date
+			FROM `tabTDS Receipt Update` t INNER JOIN `tabTDS Remittance Item` ti ON t.name = ti.parent
+			WHERE t.docstatus = 1 AND t.purpose = 'Other Invoice' {condition}
+		'''.format(condition=conditions), as_dict=True):
+		match d.invoice_type:
+			case "Purchase Invoice":
+				d.update({
+					"tds_rate": frappe.db.get_value("Purchase Taxes and Charges",{"parent":d.invoice_no},"rate")
+				})
+			case "Journal Entry":
+				d.update({
+					"tds_rate": frappe.db.get_value("Journal Entry Account",{"parent":d.invoice_no,"tax_amount":(">",0)},"rate")
+				})
+			case "Payment Entry":
+				d.update({
+					"tds_rate": frappe.db.get_value("Advance Taxes and Charges",{"parent":d.invoice_no,"tax_amount":(">",0)},"rate")
+				})
+			case "Transporter Invoice":
+				d.update({
+					"tds_rate": frappe.db.get_value("Transporter Invoice Deduction",{"parent":d.invoice_no,"amount":(">",0),"deduction_type":"TDS Deduction"},"percent")
+				})
+			case "EME Invoice":
+				d.update({
+					"tds_rate": frappe.db.get_value("EME Invoice",{"tds_amount":(">",0),"name":d.invoice_no},"tds_percent")
+				})
+		data.append(d)
+	return data
+def get_condition(filters):
+	conditions = []
+	if filters.get("party_type"):
+		conditions.append("ti.party_type = '{}'".format(filters.get("party_type")))
+	if filters.get("supplier") and filters.get("party_type") == "Supplier":
+		conditions.append("ti.party = '{}'".format(filters.get("supplier")))
+	if filters.get("customer") and filters.get("party_type") == "Customer":
+		conditions.append("ti.party = '{}'".format(filters.get("supplier")))
+	if filters.get("from_date") > filters.get("to_date"):
+		frappe.throw("To Date cannot be greater than From Date")
+	if filters.get("from_date") and filters.get("to_date"):
+		conditions.append("ti.posting_date between '{}' and '{}'".format(filters.get("from_date"),filters.get("to_date")))
+
+	return "and {}".format(" and ".join(conditions)) if conditions else ""
+def get_columns():
 	return [
 		{
 			"fieldname":"posting_date",
@@ -27,46 +65,46 @@ def get_columns(currency_symbol):
 			"width":120
 		},
 		{
-			"fieldname":"tds_taxable_amount",
-			"label":_("Gross Amount(" + currency_symbol + ")"),
-			"fieldtype":"Float",
+			"fieldname":"bill_amount",
+			"label":_("Bill Amount"),
+			"fieldtype":"Currency",
 			"width":140
 		},
 		{
 			"fieldname":"tds_rate",
 			"label":_("TDS Rate(%)"),
-			"fieldtype":"Data",
-			"width":100
+			"fieldtype":"Percent",
+			"width":130
 		},
 		{
 			"fieldname":"tds_amount",
-			"label":_("TDS Amount(" + currency_symbol + ")"),
-			"fieldtype":"Float",
+			"label":_("TDS Amount"),
+			"fieldtype":"Currency",
 			"width":140
 		},
 		{
-			"fieldname":"cheque_number",
+			"fieldname":"cheque_no",
 			"label":("Cheque Number"),
 			"fieldtype":"Data",
-			"width":100
+			"width":140
 		},
 		{
 			"fieldname":"cheque_date",
 			"label":("Cheque Date"),
 			"fieldtype":"Date",
-			"width":100
+			"width":120
 		},
 		{
-			"fieldname":"receipt_number",
+			"fieldname":"tds_receipt_number",
 			"label":("Receipt Number"),
 			"fieldtype":"Data",
-			"width":100
+			"width":130
 		},
 		{
-			"fieldname":"receipt_date",
+			"fieldname":"tds_receipt_date",
 			"label":("Receipt Date"),
 			"fieldtype":"Date",
-			"width":100,
+			"width":120,
 		}
 	]
 def calculate_total(data):
@@ -80,9 +118,6 @@ def calculate_total(data):
 	})
 	return row
 
-def get_data(filters):
-	pass
-	
 def get_datax(filters):
 	query = query1 = ''
 	je_entries = []
@@ -94,7 +129,7 @@ def get_datax(filters):
 							FROM `tabPurchase Invoice` AS a
 							inner join `tabPurchase Taxes and Charges` t 
 							on a.name = t.parent
-							inner join `tabTDS Receipt Entry` AS b
+							inner join `tabRRCO Receipt Entries` AS b
 							on a.name = b.purchase_invoice
 							AND a.posting_date BETWEEN '{0}' AND '{1}'
 							AND a.supplier = '{2}'
@@ -115,7 +150,7 @@ def get_datax(filters):
 					FROM `tabPayment Entry` as t
 					inner join `tabPurchase Taxes and Charges` t1
 					on t.name = t1.parent
-					inner join `tabTDS Receipt Entry` AS b
+					inner join `tabRRCO Receipt Entries` AS b
 					on t.name = b.purchase_invoice
 					AND t.posting_date BETWEEN '{0}' AND '{1}'
 					AND t.party = '{2}'
@@ -131,10 +166,10 @@ def get_datax(filters):
 							then di.tds_amount
 							else di.base_tds_amount end
 						as tds_amount,
-						(select cheque_number from `tabTDS Receipt Entry` rr where rr.purchase_invoice = d.name limit 1) as cheque_number,
-						(select cheque_date from `tabTDS Receipt Entry` rr where rr.purchase_invoice = d.name limit 1) as cheque_date,
-						(select receipt_number from `tabTDS Receipt Entry` rr where rr.purchase_invoice = d.name limit 1) as receipt_number,
-						(select receipt_date from `tabTDS Receipt Entry` rr where rr.purchase_invoice = d.name limit 1) as receipt_date
+						(select cheque_number from `tabRRCO Receipt Entries` rr where rr.purchase_invoice = d.name limit 1) as cheque_number,
+						(select cheque_date from `tabRRCO Receipt Entries` rr where rr.purchase_invoice = d.name limit 1) as cheque_date,
+						(select receipt_number from `tabRRCO Receipt Entries` rr where rr.purchase_invoice = d.name limit 1) as receipt_number,
+						(select receipt_date from `tabRRCO Receipt Entries` rr where rr.purchase_invoice = d.name limit 1) as receipt_date
 					FROM 
 						`tabDirect Payment` AS d
 					INNER JOIN 
@@ -142,7 +177,7 @@ def get_datax(filters):
 					WHERE d.posting_date BETWEEN '{0}' AND '{1}'
 					AND 
 						di.party = '{2}'
-					AND EXISTS(select 1 from `tabTDS Receipt Entry` rr where rr.purchase_invoice = d.name and supplier = '{2}')
+					AND EXISTS(select 1 from `tabRRCO Receipt Entries` rr where rr.purchase_invoice = d.name and supplier = '{2}')
 					AND d.currency = '{3}'
 					""".format(filters.from_date, filters.to_date, filters.supplier, filters.currency)
 		je_entries = get_journal_entries(filters)
@@ -155,7 +190,7 @@ def get_datax(filters):
 					INNER JOIN 
 						`tabBTL Sales Item` i ON b.name = i.parent
 					INNER JOIN
-						`tabTDS Receipt Entry` AS rr ON rr.purchase_invoice = b.name
+						`tabRRCO Receipt Entries` AS rr ON rr.purchase_invoice = b.name
 					WHERE b.required_commission = 1 
 					AND b.docstatus = 1
 					AND b.posting_date BETWEEN '{0}' AND '{1}'
@@ -168,7 +203,7 @@ def get_datax(filters):
 							rr.cheque_date, rr.receipt_number, rr.receipt_date  
 					FROM `tabBilling Data` i 
 					INNER JOIN
-					`tabTDS Receipt Entry` AS rr ON rr.purchase_invoice = i.parent 
+					`tabRRCO Receipt Entries` AS rr ON rr.purchase_invoice = i.parent 
 					WHERE i.parenttype = 'Airtime Collections'
 					AND i.docstatus = 1
 					AND i.tds > 0
@@ -201,7 +236,7 @@ def get_journal_entries(filters):
 						OR t1.credit_in_currency = ROUND(t2.debit_in_currency*5/100,2)
 						OR t1.credit_in_currency = ROUND(t2.debit_in_currency*10/100,2)
 					)
-			INNER JOIN `tabTDS Receipt Entry` AS rr ON rr.purchase_invoice = t.name
+			INNER JOIN `tabRRCO Receipt Entries` AS rr ON rr.purchase_invoice = t.name
 			WHERE t.posting_date BETWEEN '{from_date}' AND '{to_date}' 
 			AND t.docstatus = 1
 			AND t1.credit > 0
