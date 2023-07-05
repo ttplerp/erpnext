e170c14b7d1512a51cbf38380310423e816ab0bb
jai <jai>
2023-07-05 17:10:11 +0600

    budget duplicate validateion

--- erpnext/budget/doctype/budget/budget.py
+++ erpnext/budget/doctype/budget/budget.py
@@ -48,17 +48,17 @@ class Budget(Document):
 				b.name, ba.account from `tabBudget` b, `tabBudget Account` ba
 			where
 				ba.parent = b.name and b.docstatus < 2 and b.company = %s and %s=%s and
-				b.fiscal_year=%s and b.name != %s and ba.account in (%s) """
-			% ("%s", budget_against_field, "%s", "%s", "%s", ",".join(["%s"] * len(accounts))),
-			(self.company, budget_against, self.fiscal_year, self.name) + tuple(accounts),
+				b.fiscal_year=%s and b.name != %s and b.business_activity = %s and ba.account in (%s) """
+			% ("%s", budget_against_field, "%s", "%s", "%s", "%s", ",".join(["%s"] * len(accounts))),
+			(self.company, budget_against, self.fiscal_year, self.name, self.business_activity) + tuple(accounts),
 			as_dict=1,
 		)
 
 		for d in existing_budget:
 			frappe.throw(
 				_(
-					"Another Budget record '{0}' already exists against {1} '{2}' and account '{3}' for fiscal year {4}"
-				).format(d.name, self.budget_against, budget_against, d.account, self.fiscal_year),
+					"Another Budget record '{0}' already exists against {1} '{2}' and account '{3}' for fiscal year {4} and business activity {5}"
+				).format(d.name, self.budget_against, budget_against, d.account, self.fiscal_year, self.business_activity),
 				DuplicateBudgetError,
 			)
 
