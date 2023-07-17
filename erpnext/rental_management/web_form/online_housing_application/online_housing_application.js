8cb4849b5b394e18cc7879454a76ec130c0f69d9
thukten <thukday@gmail.com>
2023-07-17 11:53:13 +0600

    Online Housing Application

--- erpnext/rental_management/web_form/online_housing_application/online_housing_application.js
+++ erpnext/rental_management/web_form/online_housing_application/online_housing_application.js
@@ -1,3 +1,25 @@
 frappe.ready(function() {
 	// bind events here
-})
+	frappe.web_form.on('cid', (field, value) => {
+		if(value.length == "11"){
+			// Example frappe.call usage
+			frappe.call({
+				method: 'erpnext.rental_management.doctype.api_setting.api_setting.get_cid_detail',
+				args: {
+					cid: value,
+				},
+				callback: function(r) {
+					console.log(r.message);
+					// Handle the response from the server
+					if(r.message) {
+						const resultField = $('[data-fieldname="applicant_name"]');
+                		resultField.val(r.message.name);
+					}else{
+						frappe.throw("No such CID details found")
+					}
+				},
+			});
+		}
+	}); 
+});
+
--- erpnext/rental_management/web_form/online_housing_application/online_housing_application.py
+++ erpnext/rental_management/web_form/online_housing_application/online_housing_application.py
@@ -1,5 +1,9 @@
 import frappe
+from frappe.model.document import Document
+from frappe.utils import add_to_date, get_last_day, flt, getdate, cint
+from frappe import _
 
 def get_context(context):
 	# do your magic here
 	pass
+
