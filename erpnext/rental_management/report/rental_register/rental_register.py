a342951f7f2eb8ca1bcd6b7c0c91062192d950df
jai <jai>
2023-07-23 19:43:30 +0600

    rental register report

--- erpnext/rental_management/report/rental_register/rental_register.py
+++ erpnext/rental_management/report/rental_register/rental_register.py
@@ -8,49 +8,225 @@ from frappe import msgprint, _
 def execute(filters=None):
 	columns = get_columns()
 	queries = construct_query(filters);
-	data = get_data(queries)
-	return columns, data
+	# data = get_data(queries)
+	return columns, queries
 
 def get_columns():
-	return [
-	("Tenant Code") + ":Data:120",
-	("Tenant Name") + ":Data:120",
-	("CID No.") + ":Data:120",
-	("Dzongkhag") +":Data:100",
-	("Dungkhag") +":Data:90",
-	("Month") + " :Data:80",
-	("Posting Date") + " :Date:80",
-	("Fiscal Year") + ":Data:80",
-	# ("Actual Rent Amount") + ":Currency:120",
-	# ("Adjusted Amount") + ":Currency:100",
-	# ("Bill Amount") + ":Currency:120",
-	("Amount Received") + ":Currency:120",
-	("Pre-rent Received") + ":Currency:120",
-	("Excess Amount Received") + ":Currency:120",
-	("Balance Rent Amount (Due)") + ":Currency:120",
-	("Discount Amount") + ":Currency:120",
-	("Late Payment Penalty") + ":Currency:120",
-	("TDS  Deducted") + ":Currency:120",
-	("Rent Write-off") + ":Currency:120",
-	("Total Amount Received") + ":Currency:120",
-	("Rental Income") + ":Currency:120",
-	("Rental Bill ID") + ":Link/Rental Bill:130",
-	("Payment ID") + ":Data:120",
-	("Status") + ":Data:90",
-
-	("Location")+":Data:100",
-	("Building Category") +":Data:100",
-	("Department") + ":Data:100",
-	("Block No.") + ":Data:120",
-	("Flat No. ") + ":Data:120",
-	# ("Building Classification") + ":Data:120",
-	("Ministry/Agency.") + ":Data:120",
-	("Payment Mode") + ":Data:90",
-	("Rental Official Name") + ":Data:90",
-	# ("Bill No") + ":Link/Rental Bill:130",
-	# ("Penalty")+ ":Currency:120",
-	("Town Category") +":Data:100",
+	columns = [
+		{
+			"label": _("Tenant Code"),
+			"fieldname": "tenant",
+			"fieldtype": "Data",
+			"width": 120,
+		},
+		{
+			"label": _("Tenant"),
+			"fieldname": "tenant_name",
+			"fieldtype": "Data",
+			"width": 150,
+		},
+		{
+			"label": _("CID"),
+			"fieldname": "cid",
+			"fieldtype": "Data",
+			"width": 120,
+		},
+		{
+			"label": _("Dzongkhag"),
+			"fieldname": "dzongkhag",
+			"fieldtype": "Data",
+			"width": 100,
+		},
+		{
+			"label": _("Month"),
+			"fieldname": "month",
+			"fieldtype": "Data",
+			"width": 80,
+		},
+		{
+			"label": _("Posting Date"),
+			"fieldname": "posting_date",
+			"fieldtype": "Date",
+			"width": 80,
+		},
+		{
+			"label": _("Fiscal Year"),
+			"fieldname": "fiscal_year",
+			"fieldtype": "Data",
+			"width": 80,
+		},
+		{
+			"label": _("Amount Received"),
+			"fieldname": "rent_received",
+			"fieldtype": "Currency",
+			"width": 120,
+		},
+		{
+			"label": _("Pre-rent"),
+			"fieldname": "pre_rent_amount",
+			"fieldtype": "Currency",
+			"width": 120,
+		},
+		{
+			"label": _("Excess Amount"),
+			"fieldname": "excess_amount",
+			"fieldtype": "Currency",
+			"width": 120,
+		},
+		{
+			"label": _("Balance Rent (Due)"),
+			"fieldname": "balance_rent",
+			"fieldtype": "Currency",
+			"width": 120,
+		},
+		{
+			"label": _("Discount Amount"),
+			"fieldname": "discount_amount",
+			"fieldtype": "Currency",
+			"width": 120,
+		},
+		{
+			"label": _("Late Payment Penalty"),
+			"fieldname": "penalty",
+			"fieldtype": "Currency",
+			"width": 120,
+		},
+		{
+			"label": _("TDS Deducted"),
+			"fieldname": "tds_amount",
+			"fieldtype": "Currency",
+			"width": 120,
+		},
+		{
+			"label": _("Rent Write-Off"),
+			"fieldname": "rent_write_off",
+			"fieldtype": "Currency",
+			"width": 120,
+		},
+		{
+			"label": _("Total Amount Received"),
+			"fieldname": "total_amount_received",
+			"fieldtype": "Currency",
+			"width": 120,
+		},
+		{
+			"label": _("Bill ID"),
+			"fieldname": "rental_bill",
+			"fieldtype": "Data",
+			"width": 100,
+		},
+		{
+			"label": _("Payment ID"),
+			"fieldname": "rental_payment",
+			"fieldtype": "data",
+			"width": 120,
+		},
+		{
+			"label": _("Status"),
+			"fieldname": "docstatus",
+			"fieldtype": "data",
+			"width": 80,
+		},
+		{
+			"label": _("Location"),
+			"fieldname": "location",
+			"fieldtype": "data",
+			"width": 80,
+		},
+		{
+			"label": _("Building Category"),
+			"fieldname": "building_category",
+			"fieldtype": "data",
+			"width": 80,
+		},
+		{
+			"label": _("Department"),
+			"fieldname": "department",
+			"fieldtype": "data",
+			"width": 80,
+		},
+		{
+			"label": _("Block No."),
+			"fieldname": "block_no",
+			"fieldtype": "data",
+			"width": 80,
+		},
+		{
+			"label": _("Flat No."),
+			"fieldname": "flat_no",
+			"fieldtype": "data",
+			"width": 80,
+		},
+		{
+			"label": _("Ministry/Agency"),
+			"fieldname": "ministry_agency",
+			"fieldtype": "data",
+			"width": 100,
+		},
+		{
+			"label": _("Payment Mode"),
+			"fieldname": "payment_mode",
+			"fieldtype": "data",
+			"width": 100,
+		},
+		{
+			"label": _("Focal Person"),
+			"fieldname": "rental_official_name",
+			"fieldtype": "data",
+			"width": 150,
+		},
+		{
+			"label": _("Town Category"),
+			"fieldname": "town_category",
+			"fieldtype": "data",
+			"width": 100,
+		},
 	]
+
+	return columns
+
+	# return [
+	# ("Tenant Code") + ":Data:120",
+	# ("Tenant Name") + ":Data:120",
+	# ("CID No.") + ":Data:120",
+	# ("Dzongkhag") +":Data:100",
+	# # ("Dungkhag") +":Data:90",
+	# ("Month") + " :Data:80",
+	# ("Posting Date") + " :Date:80",
+	# ("Fiscal Year") + ":Data:80",
+	# # ("Actual Rent Amount") + ":Currency:120",
+	# # ("Adjusted Amount") + ":Currency:100",
+	# # ("Bill Amount") + ":Currency:120",
+	# ("Amount Received") + ":Currency:120",
+	# ("Pre-rent Received") + ":Currency:120",
+	# ("Excess Amount Received") + ":Currency:120",
+	# ("Balance Rent Amount (Due)") + ":Currency:120",
+	# ("Discount Amount") + ":Currency:120",
+	# ("Late Payment Penalty") + ":Currency:120",
+	# ("TDS  Deducted") + ":Currency:120",
+	# ("Rent Write-off") + ":Currency:120",
+	# ("Total Amount Received") + ":Currency:120",
+	# ("Rental Income") + ":Currency:120",
+	# ("Rental Bill ID") + ":Link/Rental Bill:130",
+	# ("Payment ID") + ":Data:120",
+	# ("Status") + ":Data:90",
+
+	# ("Location")+":Data:100",
+	# ("Building Category") +":Data:100",
+	# ("Department") + ":Data:100",
+	# ("Block No.") + ":Data:120",
+	# ("Flat No. ") + ":Data:120",
+	# # ("Building Classification") + ":Data:120",
+	# ("Ministry/Agency.") + ":Data:120",
+	# ("Payment Mode") + ":Data:90",
+	# ("Rental Official Name") + ":Data:90",
+	# # ("Bill No") + ":Link/Rental Bill:130",
+	# # ("Penalty")+ ":Currency:120",
+	# ("Town Category") +":Data:100",
+	# ]
+
+# def get_data(query):
+	
 
 def construct_query(filters):
 	status = ""
@@ -63,7 +239,7 @@ def construct_query(filters):
 		SELECT
 			rb.tenant tenant,
 			rb.tenant_name tenant_name,
-			rb.cid cid,
+			rb.tenant_cid cid,
 			rb.dzongkhag dzongkhag,
 			rb.month month,
 			rpi_parent.posting_date posting_date,
@@ -101,18 +277,12 @@ def construct_query(filters):
 			rb.flat_no flat_no,
 			rb.ministry_agency ministry_agency,
 			rpi_parent.payment_mode payment_mode,
-			rpi_parent.rental_official_name rental_official_name,
+			rb.focal_name rental_official_name,
 			rb.town_category town_category
 		FROM
 			`tabRental Bill` as rb
-		LEFT JOIN
-			`tabRental Payment Item` as rpi
-		ON
-			rb.name = rpi.rental_bill AND rpi.docstatus = 1
-		LEFT JOIN
-			`tabRental Payment` as rpi_parent
-		ON
-			rpi.parent = rpi_parent.name AND rpi_parent.docstatus = 1
+			LEFT JOIN `tabRental Payment Item` as rpi ON rb.name = rpi.rental_bill AND rpi.docstatus = 1
+			LEFT JOIN `tabRental Payment` as rpi_parent ON rpi.parent = rpi_parent.name AND rpi_parent.docstatus = 1
 		WHERE
 			{status}
 			""".format(
@@ -147,10 +317,11 @@ def construct_query(filters):
 		query += " and rb.rental_focal = '{0}'".format(filters.get("rental_official"))
 
 	if filters.get("building_classification"):
-		query += " and building_classification = '{0}'".format(filters.get("building_classification"))
+		query += " and rb.building_classification = '{0}'".format(filters.get("building_classification"))
 		
 	if filters.get("from_month") and filters.get("to_month"):
 		query += " and rb.month between {0} and {1}".format(filters.get("from_month"),filters.get("to_month"))
 	query += " ORDER BY rb.tenant, rb.month"
-	# return frappe.db.sql(query)
-	return query
+
+	return frappe.db.sql(query, as_dict=True)
+	# return query
