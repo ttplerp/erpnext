a0fe5d3a29e97449909fe05231618ec8a3b6a746
Dendup67 <dendup1998@gmail.com>
2023-08-04 13:02:10 +0600

    Account query in budget reappropiation

--- erpnext/budget/doctype/budget_reappropiation/budget_reappropiation.js
+++ erpnext/budget/doctype/budget_reappropiation/budget_reappropiation.js
@@ -29,8 +29,7 @@ var apply_account_filter = function(frm){
 			filters: {
 				company: frm.doc.company,
 				is_group: 0,
-				account_type:["in",["Expense Account","Fixed Asset"]],
-				budget_type:frm.doc.budget_type
+				root_type:["in",["Expense","Asset"]],
 			}
 		};
 	});
