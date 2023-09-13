03eb0e5c3a5c5a300ffd3a3a516580399d73bfff
thukten <thukday@gmail.com>
2023-09-13 14:15:53 +0600

    Bank Statemennt File

--- erpnext/accounts/doctype/bank_statement_files/bank_statement_files.py
+++ erpnext/accounts/doctype/bank_statement_files/bank_statement_files.py
@@ -10,41 +10,51 @@ import csv
 class BankStatementFiles(Document):
 	pass
 
+def is_date(string):
+    try:
+        # Attempt to parse the string as a date
+        datetime.strptime(string, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
 @frappe.whitelist()
 def update_brs():
-	from datetime import datetime
 	for a in frappe.db.sql("""
 						SELECT name, bank_statement_file
 						FROM `tabBank Statement Files`
 						WHERE update_brs = 0
 						ORDER BY download_date
 					""",as_dict=True):
-		if os.path.exists(a.bank_statement_file):
-			with open(a.bank_statement_file,'r') as f:
-				reader = csv.reader(f)
-				row=1
-				for i in reader:
-					if row > 1:
-						date_string = str(i[0])
-						date_str = date_string.split("-")
-						if len(date_str[2]) == 4:
-							datetime_obj = datetime.strptime(date_string,'%d-%b-%Y')
-						else:
-							datetime_obj = datetime.strptime(date_string,'%d-%b-%y')
-						datetime_obj = datetime.strptime(date_string,'%d-%b-%Y')
-						correct_date_format = datetime_obj.strftime('%Y-%m-%d')
-						doc = frappe.get_doc({
-								'doctype' : "BRS Entries",
-								'clearing_date': correct_date_format,
-								'jrnl_no' : i[1],
-								'clearing_time' : i[2],
-								'account_no' : i[3],
-								'amount' : i[4],
-								'ref_no' : i[5],
-								'narration' : i[6],
-								'cheque_no' : i[1],
-						})
-						doc.submit()
-					row += 1
-		frappe.db.sql("update `tabBank Statement Files` set update_brs=1 where name='{}'".format(a.name))
+		try:
+			if os.path.exists(a.bank_statement_file):
+				with open(a.bank_statement_file,'r') as f:
+					reader = csv.reader(f)
+					row=1
+					for i in reader:
+						if row > 1:
+							k=0 if is_date(str(i[0])) else 1
+							date_string = str(i[0+k])
+							date_str = date_string.split("-")
+							if len(date_str[2]) == 4:
+								datetime_obj = datetime.strptime(date_string,'%d-%b-%Y')
+							else:
+								datetime_obj = datetime.strptime(date_string,'%d-%b-%y')
+							correct_date_format = datetime_obj.strftime('%Y-%m-%d')
+							doc = frappe.get_doc({
+									'doctype' : "BRS Entries",
+									'clearing_date': correct_date_format,
+									'jrnl_no' : i[1+k],
+									'clearing_time' : i[2+k],
+									'account_no' : i[3+k],
+									'amount' : i[4+k],
+									'ref_no' : i[5+k],
+									'narration' : i[6+k],
+									'cheque_no' : i[1+k],
+							})
+							doc.submit()
+						row += 1
+			frappe.db.sql("update `tabBank Statement Files` set update_brs=1 where name='{}'".format(a.name))
+		except Exception as e:
+			logging.critical("FAILUER TO INSERT IN BRS Entries for file {}".format(a.bank_statement_file))
 	frappe.db.commit()
