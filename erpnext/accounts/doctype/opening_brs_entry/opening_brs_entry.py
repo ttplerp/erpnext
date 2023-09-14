ae35dfc244b3a5a2e5ce178af6d43a71dac787e3
Dendup67 <dendup1998@gmail.com>
2023-09-14 10:24:01 +0600

    BRS entry

--- erpnext/accounts/doctype/opening_brs_entry/opening_brs_entry.py
+++ erpnext/accounts/doctype/opening_brs_entry/opening_brs_entry.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from frappe import _
+from frappe.utils import cint, cstr, flt, formatdate, getdate, now
 
 class OpeningBRSEntry(Document):
 	def validate(self):
@@ -12,5 +13,5 @@ class OpeningBRSEntry(Document):
 
 	def validate_amount(self):
 		for d in self.details:
-			if(float(d.credit) > 0 and float(d.debit) > 0):
+			if(flt(d.credit) > 0 and flt(d.debit) > 0):
 				frappe.throw(_("At Row {0} either Credit Amount should be 0 or Debit Amount should be 0 for party {1}").format(d.idx, d.party))
