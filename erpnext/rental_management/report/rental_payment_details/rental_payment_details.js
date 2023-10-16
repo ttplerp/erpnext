e8d3a401fe8f494758e9a95b5647ad86f6e0b1ea
Rinzin <rinzinn1996@gmail.com>
2023-10-16 14:08:05 +0600

    changes in report

--- erpnext/rental_management/report/rental_payment_details/rental_payment_details.js
+++ erpnext/rental_management/report/rental_payment_details/rental_payment_details.js
@@ -10,5 +10,33 @@ frappe.query_reports["Rental Payment Details"] = {
 			"fieldtype": "Link",
 			"options": "Branch"
 		},
+		{
+			fieldname: "fiscal_year",
+			label: __("Fiscal Year"),
+			fieldtype: "Link",
+			options: "Fiscal Year",
+			default: frappe.defaults.get_user_default("fiscal_year"),
+			reqd: 1,
+		},
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": get_today(),
+		},
+		{
+			fieldname: "month",
+			label: "Month",
+			fieldtype: "Select",
+			options: ["","01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+			default: "",
+		},
+		
 	]
 };
--- erpnext/rental_management/report/rental_payment_details/rental_payment_details.py
+++ erpnext/rental_management/report/rental_payment_details/rental_payment_details.py
@@ -18,21 +18,21 @@ def get_columns(data):
 			"label": "Rental Payment ID",
 			"fieldtype": "Link",
 			"options": "Rental Payment",
-			"width": 100
+			"width": 200
 		},
 		{
 			"fieldname": "branch",
 			"label": "Branch",
 			"fieldtype": "Link",
 			"options": "Branch",
-			"width": 150
+			"width": 200
 		},
 		{
 			"fieldname": "tenant",
 			"label": "Tenant",
 			"fieldtype": "Link",
 			"options": "Tenant Information",
-			"width": 120
+			"width": 100
 		},
 		{
 			"fieldname": "tenant_name",
@@ -44,55 +44,61 @@ def get_columns(data):
 			"fieldname": "posting_date",
 			"label": "Posting Date",
 			"fieldtype": "Date",
+			"width": 120
+		},
+		{
+			"fieldname": "month",
+			"label": "Month",
+			"fieldtype": "Data",
 			"width": 70
 		},
 		{
 			"fieldname": "payment_mode",
 			"label": "Payment Mode",
 			"fieldtype": "Data",
-			"width":70
+			"width":120
 		},
 		{
 			"fieldname": "tds_amount",
 			"label": "TDS Amount",
 			"fieldtype": "Data",
-			"width":70
+			"width":120
 		},
 		{
 			"fieldname": "penalty_amount",
 			"label": "Penalty Amount",
 			"fieldtype": "Data",
-			"width":70
+			"width":130
 		},
 		{
 			"fieldname": "pre_rent_amount",
 			"label": "Pre-Rent Amount",
 			"fieldtype": "Data",
-			"width":70
+			"width":150
 		},
 		{
 			"fieldname": "excess_amount",
 			"label": "Excess Amount",
 			"fieldtype": "Data",
-			"width":70
+			"width":120
 		},
 		{
 			"fieldname": "total_bill_amount",
 			"label": "Total Bill Amount",
 			"fieldtype": "Data",
-			"width":100
+			"width":150
 		},
 		{
 			"fieldname": "total_amount_received",
 			"label": "Total Amount Received",
 			"fieldtype": "Data",
-			"width":70
+			"width":180
 		},
 		{
 			"fieldname": "outstanding_amount",
 			"label": "Outstanding Amount",
 			"fieldtype": "Data",
-			"width":70
+			"width":160
 		},
 		
 	]
@@ -101,39 +107,53 @@ def construct_query(filters=None):
 	conditions, filters = get_conditions(filters)
 	query = ("""
 			select 
-				r.name as name, r.branch as branch, r.tenant as tenant,r.tenant_name as tenant_name, r.posting_date as posting_date, r.payment_mode as payment_mode, r.tds_amount as tds_amount, r.penalty_amount as penalty_amount, r.pre_rent_amount as pre_rent_amount, r.excess_amount as excess_amount, r.total_bill_amount as total_bill_amount, r.total_amount_received as total_amount_received
+				r.name as name, r.branch as branch, r.posting_date as posting_date, r.payment_mode as payment_mode
 			from `tabRental Payment` r
 			where r.docstatus = 1
 			{}
 			""".format(conditions))
+	
 	return query	
 def get_data(query, filters):
-	conditions = ""
+	cond = ""
+	if filters.get("month"):
+		cond += """and ri.month ='{}'""".format(filters.get("month"))
 	data = []
 	datas = frappe.db.sql(query, as_dict=True)
 	for d in datas:
-		outstanding = flt(d.total_amount_received)-flt(d.total_bill_amount)
-		row = {
-			"rp_id":d.name,
-			"branch":d.branch,
-			"tenant":d.tenant,
-			"tenant_name": d.tenant_name,
-			"posting_date": d.posting_date,
-			"payment_mode": d.payment_mode,
-			"tds_amount": d.tds_amount,
-			"penalty_amount":d.penalty_amount,
-			"pre_rent_amount": d.pre_rent_amount,
-			"excess_amount":d.excess_amount,
-			"total_bill_amount":d.total_bill_amount,
-			"total_amount_received": d.total_amount_received,
-			"outstanding_amount":outstanding
-		}
-		data.append(row)
+		rental_item = frappe.db.sql("""
+			select 
+				ri.tenant as tenant,ri.tenant_name as tenant_name, ri.month as month, ri.tds_amount as tds_amount, ri.penalty as penalty, ri.pre_rent_amount as pre_rent_amount, ri.excess_amount as excess_amount, ri.bill_amount as bill_amount, ri.total_amount_received as total_amount_received, ri.balance_rent as balance_rent
+			from `tabRental Payment Item` ri 
+			where ri.parent = '{0}'
+			{1}
+			""".format(d.name, cond), as_dict=True)
+		for raw in rental_item:
+			row = {
+				"rp_id":d.name,
+				"branch":d.branch,
+				"tenant":raw.tenant,
+				"tenant_name": raw.tenant_name,
+				"posting_date": d.posting_date,
+				"month": raw.month,
+				"payment_mode": d.payment_mode,
+				"tds_amount": raw.tds_amount,
+				"penalty_amount":raw.penalty,
+				"pre_rent_amount": raw.pre_rent_amount,
+				"excess_amount":raw.excess_amount,
+				"total_bill_amount":raw.bill_amount,
+				"total_amount_received": raw.total_amount_received,
+				"outstanding_amount":raw.balance_rent
+			}
+			data.append(row)
 	return data
 def get_conditions(filters):
 	conditions = ""
 	if filters.get("branch"):
 		conditions += """and r.branch ='{}'""".format(filters.get("branch"))
-
+	if filters.get("fiscal_year"):
+		conditions += """and r.fiscal_year ='{}'""".format(filters.get("fiscal_year"))
+	if filters.get("from_date") and filters.get("to_date"):
+		conditions += """and r.posting_date between '{from_date}' and '{to_date}'""".format(from_date=filters.get("from_date"),to_date=filters.get("to_date"))
 	return conditions, filters
 
