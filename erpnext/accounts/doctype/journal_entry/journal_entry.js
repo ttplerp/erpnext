1f9cd7377f04e97724e5a82a860e209a9d9e2d0d
thukten <thukday@gmail.com>
2023-10-25 13:11:31 +0600

    Utility Bill Payment

--- erpnext/accounts/doctype/journal_entry/journal_entry.js
+++ erpnext/accounts/doctype/journal_entry/journal_entry.js
@@ -821,7 +821,7 @@ $.extend(erpnext.journal_entry, {
 /* ePayment Begins */
 var create_custom_buttons = function(frm){
 	if(frm.doc.docstatus == 1 && (frm.doc.voucher_type == "Bank Entry" || frm.doc.voucher_type == "Contra Entry")){
-		if(!frm.doc.bank_payment || frm.doc.payment_status == 'Failed' || frm.doc.payment_status == 'Payment Failed'){
+		if(frm.doc.payment_status == 'Failed' || frm.doc.payment_status == 'Payment Failed'){
 			frm.page.set_primary_action(__('Process Payment'), () => {
 				frappe.model.open_mapped_doc({
 					method: "erpnext.accounts.doctype.journal_entry.journal_entry.make_bank_payment",
