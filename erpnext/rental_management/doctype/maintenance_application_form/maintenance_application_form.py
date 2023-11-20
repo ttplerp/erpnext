30b984aa656651505dd003ed1312c9656867160f
Rinzin <rinzinn1996@gmail.com>
2023-11-20 12:37:26 +0600

    ignore permission

--- erpnext/rental_management/doctype/maintenance_application_form/maintenance_application_form.py
+++ erpnext/rental_management/doctype/maintenance_application_form/maintenance_application_form.py
@@ -62,7 +62,7 @@ def get_cid_detail(tenant_cid):
     try:
         # Execute SQL query
         sql_query = """
-        SELECT name, tenant_name, block_no, flat_no, location_name,dzongkhag,locations,phone_no, name
+        SELECT name, tenant_name, block_no, flat_no, location_name,dzongkhag,locations,phone_no, name,tenant_cid
         FROM `tabTenant Information` 
         WHERE tenant_cid = %(tenant_cid)s
         """
@@ -71,7 +71,7 @@ def get_cid_detail(tenant_cid):
 
         data = frappe.db.sql(sql_query, query_params, as_dict=True)
 		
-        # Convert data to JSON format
+       
         
 
         # Return the data as JSON to the client side
@@ -82,4 +82,58 @@ def get_cid_detail(tenant_cid):
 
 @frappe.whitelist(allow_guest=True)
 def checkCidExistence(tenant_cid):
-    return True
+    
+    try:
+        # Execute SQL query
+        sql_query = """
+        SELECT name, maf_status
+        FROM `tabMaintenance Application Form` 
+        WHERE cidd = %(tenant_cid)s
+        ORDER BY creation DESC LIMIT 1
+        
+        """
+
+        # Parameters to pass to the query
+        query_params = {"tenant_cid": tenant_cid}
+
+        # Fetch data using frappe.db.sql
+        data = frappe.db.sql(sql_query, query_params, as_dict=True)
+
+        if data:
+            first_record = data[0]  # Accessing the first record
+            maf_status = first_record.get('maf_status')  # Retrieving the 'name' field
+            # frappe.throw(record_name)
+        
+        # Check if data exists
+        if data and maf_status!='Closed':
+           
+            # CID exists, showing the existing CID and allowing the check
+            frappe.msgprint(f"CID {tenant_cid} already applied and that application is not yet closed.")
+            return True
+        else:
+            # CID does not exist, preventing the check and showing a message
+           
+            return False
+        
+         # Check if data exists
+        # if data and maf_status!='closed':
+        #     # CID exists, showing the existing CID and allowing the check
+        #     frappe.msgprint(f"CID {tenant_cid} already applied and its not closed yet. ")
+        #     return True
+        # else:
+        #     # CID does not exist, preventing the check and showing a message
+           
+        #     return False
+      
+
+    except Exception as e:
+        # Log any exceptions that occur during the execution of the query
+        frappe.log_error(_("Error in check_cid_existence: {0}").format(e))
+        return None
+
+    
+
+
+	   
+ 
+    
