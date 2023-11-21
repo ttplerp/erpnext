d01662f5a8da55fd60022d21876c7f301cff9855
Dendup67 <dendup1998@gmail.com>
2023-11-21 17:28:57 +0600

    BRS

--- erpnext/accounts/report/bank_reconciliation_statement/bank_reconciliation_statement.py
+++ erpnext/accounts/report/bank_reconciliation_statement/bank_reconciliation_statement.py
@@ -157,8 +157,8 @@ def get_payment_entries(filters):
 		select
 			"Payment Entry" as payment_document, name as payment_entry,
 			reference_no, reference_date as ref_date,
-			if(paid_to=%(account)s, received_amount, 0) as debit,
-			if(paid_from=%(account)s, paid_amount, 0) as credit,
+			if(paid_to=%(account)s, received_amount_after_tax, 0) as debit,
+			if(paid_from=%(account)s, paid_amount_after_tax, 0) as credit,
 			posting_date, ifnull(party,if(paid_from=%(account)s,paid_to,paid_from)) as against_account, clearance_date,
 			if(paid_to=%(account)s, paid_to_account_currency, paid_from_account_currency) as account_currency
 		from `tabPayment Entry`
@@ -290,7 +290,7 @@ def get_amounts_not_reflected_in_system(filters):
 
 	pe_amount = frappe.db.sql(
 		"""
-		select sum(if(paid_from=%(account)s, paid_amount, received_amount))
+		select sum(if(paid_from=%(account)s, paid_amount_after_tax, received_amount_after_tax))
 		from `tabPayment Entry`
 		where (paid_from=%(account)s or paid_to=%(account)s) and docstatus=1
 		and posting_date > %(report_date)s and clearance_date <= %(report_date)s""",
