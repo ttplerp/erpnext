99eb36baea6ff089c81780fdbb3a9cfe8b6af839
Tshering-Wangchuk <tsheringwangchuky@gmail.com>
2023-12-19 09:23:57 +0600

    maintenance app form email validation

--- erpnext/rental_management/doctype/maintenance_application_form/maintenance_application_form.py
+++ erpnext/rental_management/doctype/maintenance_application_form/maintenance_application_form.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+import re
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
 
@@ -11,22 +12,32 @@ class MaintenanceApplicationForm(Document):
 	def get_query(self, doctype, txt, searchfield, start, page_len, filters):
 		if searchfield == "linked_field":
 			return frappe.db.sql("""
-                SELECT name, other_field
-                FROM `tabLinkedDocType`
-                WHERE other_field LIKE %(txt)s
-                ORDER BY other_field ASC
-                LIMIT %(start)s, %(page_len)s
-            """, {
-                "txt": "%%%s%%" % txt,
-                "start": start,
-                "page_len": page_len
-            })
+				SELECT name, other_field
+				FROM `tabLinkedDocType`
+				WHERE other_field LIKE %(txt)s
+				ORDER BY other_field ASC
+				LIMIT %(start)s, %(page_len)s
+			""", {
+				"txt": "%%%s%%" % txt,
+				"start": start,
+				"page_len": page_len
+			})
 	def validate(self):
+		self.get_branch_missing()
 		if self.maf_status =="On Hold":
 			pass
 		elif self.workflow_state == "Waiting Supervisor Approval":
 			self.maf_status ="In Process"
 		
+		pattern = r'^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$'
+
+		
+		if not re.match(pattern, self.email):
+			frappe.throw("Email Format is not right") 
+	
+	def get_branch_missing(self):
+		if self.tenant_id and frappe.db.get_value("Tenant Information", self.tenant_id, "branch"):
+			self.db_set("branch", frappe.db.get_value("Tenant Information", self.tenant_id, "branch"))
 
 @frappe.whitelist()
 def make_technical_sanction(source_name, target_doc=None):
@@ -59,81 +70,81 @@ from frappe import _
 
 @frappe.whitelist(allow_guest=True)
 def get_cid_detail(tenant_cid):
-    try:
-        # Execute SQL query
-        sql_query = """
-        SELECT name, tenant_name, block_no, flat_no, location_name,dzongkhag,locations,phone_no, name,tenant_cid
-        FROM `tabTenant Information` 
-        WHERE tenant_cid = %(tenant_cid)s
-        """
-        # Parameters to pass to the query
-        query_params = {"tenant_cid": tenant_cid}
+	try:
+		# Execute SQL query
+		sql_query = """
+		SELECT name, tenant_name, block_no, flat_no, location_name,dzongkhag,locations,phone_no, name,tenant_cid
+		FROM `tabTenant Information` 
+		WHERE tenant_cid = %(tenant_cid)s
+		"""
+		# Parameters to pass to the query
+		query_params = {"tenant_cid": tenant_cid}
 
-        data = frappe.db.sql(sql_query, query_params, as_dict=True)
+		data = frappe.db.sql(sql_query, query_params, as_dict=True)
 		
-       
-        
+	   
+		
 
-        # Return the data as JSON to the client side
-        return data
-    except Exception as e:
-        frappe.log_error(_("Error in get_tenant_name: {0}").format(e))
-        return None
+		# Return the data as JSON to the client side
+		return data
+	except Exception as e:
+		frappe.log_error(_("Error in get_tenant_name: {0}").format(e))
+		return None
 
 @frappe.whitelist(allow_guest=True)
 def checkCidExistence(tenant_cid):
-    
-    try:
-        # Execute SQL query
-        sql_query = """
-        SELECT name, maf_status
-        FROM `tabMaintenance Application Form` 
-        WHERE cidd = %(tenant_cid)s
-        ORDER BY creation DESC LIMIT 1
-        
-        """
+	
+	try:
+		# Execute SQL query
+		sql_query = """
+		SELECT name, maf_status
+		FROM `tabMaintenance Application Form` 
+		WHERE cidd = %(tenant_cid)s
+		ORDER BY creation DESC LIMIT 1
+		
+		"""
 
-        # Parameters to pass to the query
-        query_params = {"tenant_cid": tenant_cid}
+		# Parameters to pass to the query
+		query_params = {"tenant_cid": tenant_cid}
 
-        # Fetch data using frappe.db.sql
-        data = frappe.db.sql(sql_query, query_params, as_dict=True)
+		# Fetch data using frappe.db.sql
+		data = frappe.db.sql(sql_query, query_params, as_dict=True)
 
-        if data:
-            first_record = data[0]  # Accessing the first record
-            maf_status = first_record.get('maf_status')  # Retrieving the 'name' field
-            # frappe.throw(record_name)
-        
-        # Check if data exists
-        if data and maf_status not in ['Closed', 'Completed']:
-           
-            # CID exists, showing the existing CID and allowing the check
-            frappe.msgprint(f"CID {tenant_cid} already applied and that application is not yet closed or completed.")
-            return True
-        else:
-            # CID does not exist, preventing the check and showing a message
-           
-            return False
-        
-         # Check if data exists
-        # if data and maf_status!='closed':
-        #     # CID exists, showing the existing CID and allowing the check
-        #     frappe.msgprint(f"CID {tenant_cid} already applied and its not closed yet. ")
-        #     return True
-        # else:
-        #     # CID does not exist, preventing the check and showing a message
-           
-        #     return False
-      
+		if data:
+			first_record = data[0]  # Accessing the first record
+			maf_status = first_record.get('maf_status')  # Retrieving the 'name' field
+			# frappe.throw(record_name)
+		
+		# Check if data exists
+		if data and maf_status not in ['Closed', 'Completed']:
+		   
+			# CID exists, showing the existing CID and allowing the check
+			frappe.msgprint(f"CID {tenant_cid} already applied and that application is not yet closed or completed.")
+			return True
+		else:
+			# CID does not exist, preventing the check and showing a message
+		   
+			return False
+		
+		 # Check if data exists
+		# if data and maf_status!='closed':
+		#     # CID exists, showing the existing CID and allowing the check
+		#     frappe.msgprint(f"CID {tenant_cid} already applied and its not closed yet. ")
+		#     return True
+		# else:
+		#     # CID does not exist, preventing the check and showing a message
+		   
+		#     return False
+	  
 
-    except Exception as e:
-        # Log any exceptions that occur during the execution of the query
-        frappe.log_error(_("Error in check_cid_existence: {0}").format(e))
-        return None
+	except Exception as e:
+		# Log any exceptions that occur during the execution of the query
+		frappe.log_error(_("Error in check_cid_existence: {0}").format(e))
+		return None
 
-    
+	
 
 
 	   
  
-    
+	
