97870919d29c6661e834ae7f30840bc15d1212cd
NYuethyue <dawanyuethyuetshering111@gmail.com>
2024-02-14 16:04:08 +0600

    pol receive

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -1183,7 +1183,7 @@ class PaymentEntry(AccountsController):
             )
         else:
             if self.payment_type in ("Pay", "Internal Transfer"):
-                if self.advances:
+                if self.advances or self.taxes:
                     balance_amount = flt(self.advances[0].balance_amount)
                     advance_account = self.advances[0].advance_account
                     if self.total_outstanding_amount > 0.00:
@@ -1232,25 +1232,7 @@ class PaymentEntry(AccountsController):
                                     else self.paid_to,
                                     "party": self.party,
                                     "party_type": "Supplier",
-                                    "credit_in_account_currency": self.paid_amount,
-                                    "credit": self.base_paid_amount,
-                                    "cost_center": self.cost_center,
-                                    "post_net_value": True,
-                                },
-                                item=self,
-                            )
-                        )
-                else:
-                    if self.taxes:
-                        gl_entries.append(
-                            self.get_gl_dict(
-                                {
-                                    "account": self.paid_from,
-                                    "account_currency": self.paid_from_account_currency,
-                                    "against": self.party
-                                    if self.payment_type == "Pay"
-                                    else self.paid_to,
-                                    "credit_in_account_currency":  flt(self.base_paid_amount)+flt(self.total_taxes_and_charges),
+                                    "credit_in_account_currency": flt(self.paid_amount)+flt(self.total_taxes_and_charges),
                                     "credit": flt(self.base_paid_amount)+flt(self.total_taxes_and_charges),
                                     "cost_center": self.cost_center,
                                     "post_net_value": True,
@@ -1258,23 +1240,41 @@ class PaymentEntry(AccountsController):
                                 item=self,
                             )
                         )
-                    else:
-                        gl_entries.append(
-                            self.get_gl_dict(
-                                {
-                                    "account": self.paid_from,
-                                    "account_currency": self.paid_from_account_currency,
-                                    "against": self.party
-                                    if self.payment_type == "Pay"
-                                    else self.paid_to,
-                                    "credit_in_account_currency": self.paid_amount,
-                                    "credit": self.base_paid_amount,
-                                    "cost_center": self.cost_center,
-                                    "post_net_value": True,
-                                },
-                                item=self,
-                            )
-                        )
+                # else:
+                #     if self.taxes:
+                #         gl_entries.append(
+                #             self.get_gl_dict(
+                #                 {
+                #                     "account": self.paid_from,
+                #                     "account_currency": self.paid_from_account_currency,
+                #                     "against": self.party
+                #                     if self.payment_type == "Pay"
+                #                     else self.paid_to,
+                #                     "credit_in_account_currency":  flt(self.base_paid_amount)+flt(self.total_taxes_and_charges),
+                #                     "credit": flt(self.base_paid_amount)+flt(self.total_taxes_and_charges),
+                #                     "cost_center": self.cost_center,
+                #                     "post_net_value": True,
+                #                 },
+                #                 item=self,
+                #             )
+                #         )
+                #     else:
+                #         gl_entries.append(
+                #             self.get_gl_dict(
+                #                 {
+                #                     "account": self.paid_from,
+                #                     "account_currency": self.paid_from_account_currency,
+                #                     "against": self.party
+                #                     if self.payment_type == "Pay"
+                #                     else self.paid_to,
+                #                     "credit_in_account_currency": self.paid_amount,
+                #                     "credit": self.base_paid_amount,
+                #                     "cost_center": self.cost_center,
+                #                     "post_net_value": True,
+                #                 },
+                #                 item=self,
+                #             )
+                #         )
             if self.payment_type in ("Receive", "Internal Transfer"):
                 if self.advances:
                     balance_amount = flt(self.advances[0].balance_amount)
