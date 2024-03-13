da354aaa4aba1bf3c0496d10f9d0452e534aa3ba
Dendup67 <dendup1998@gmail.com>
2024-03-13 12:02:15 +0600

    Revenue Target Report

--- erpnext/budget/report/revenue_target/revenue_target.js
+++ erpnext/budget/report/revenue_target/revenue_target.js
@@ -5,10 +5,12 @@
 frappe.query_reports["Revenue Target"] = {
 	"filters": [
 		{
-			"fieldname": "year",
-			"label": __("Year"),
+			"fieldname": "fiscal_year",
+			"label": __("Fiscal Year"),
 			"fieldtype": "Link",
-			"options": "Fiscal Year"
+			"options": "Fiscal Year",
+			"default": frappe.sys_defaults.fiscal_year, 
+			"reqd": 1
 		},
 		{
 			"fieldname": "cost_center",
@@ -17,40 +19,11 @@ frappe.query_reports["Revenue Target"] = {
 			"options": "Cost Center"
 		},
 		{
-			"fieldname": "from_date",
-			"label": __("From Date"),
-			"fieldtype": "Date",
-			"default": frappe.defaults.get_user_default("year_start_date"),
-		},
-		{
-			"fieldname": "to_date",
-			"label": __("To Date"),
-			"fieldtype": "Date",
-			"default": get_today(),
-		},
-		{
-			"fieldname": "monthly",
-			"label": __("Monthly"),
-			"fieldtype": "Check",
-			"default": 1,
-			"on_change": function (query_report) {
-				var monthly = query_report.get_values().monthly;
-				var month_filter = query_report.get_filter("month");
-				if (monthly) {
-					month_filter.toggle(true);
-				}
-				else {
-					month_filter.toggle(false);
-				}
-				query_report.refresh();
-			}
-		},
-		{
 			"fieldname": "month",
 			"label": __("Month"),
 			"fieldtype": "Select",
-			"width": "100",
 			"options": ["January","February","March","April","May","June","July","August","September","October","November","December"],
+			"reqd": 1
 		}
 	]
 };
--- erpnext/budget/report/revenue_target/revenue_target.py
+++ erpnext/budget/report/revenue_target/revenue_target.py
@@ -1,16 +1,19 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
+
 from frappe.utils import flt
 import frappe
+import calendar
+
 def execute(filters=None):
 	columns, data = [], []
-	columns = get_columns(data)
+	columns = get_columns()
 	queries = construct_query(filters)
 	data = get_data(queries,filters)
 	
 	return columns, data
 
-def get_columns(data):
+def get_columns():
 	return [
 		{
 			"fieldname": "cost_center",
@@ -33,61 +36,59 @@ def get_columns(data):
 			"width": 120
 		},
 		{
-			"fieldname": "initial_target",
-			"label": "Initial Target",
-			"fieldtype": "Data",
-			"width": 120
-		},
-		{
-			"fieldname": "adestment",
-			"label": "Adestment",
-			"fieldtype": "Data",
-			"width": 120
-		},
-		{
-			"fieldname": "final_target",
-			"label": "Final Target",
-			"fieldtype": "Data",
-			"width": 120
+			"fieldname": "target_amount",
+			"label": "Target Amount",
+			"fieldtype": "Currency",
+			"width": 160
 		},
 		{
 			"fieldname": "achieved_amount",
 			"label": "Achieved Amount",
-			"fieldtype": "Data",
-			"width": 120
+			"fieldtype": "Currency",
+			"width": 160
 		},
 		{
 			"fieldname": "balance_amount",
 			"label": "Balance Amount",
-			"fieldtype": "Data",
-			"width": 120
+			"fieldtype": "Currency",
+			"width": 160
 		},
 		{
 			"fieldname": "achieved_percent",
 			"label": "Achieved Percent",
-			"fieldtype": "Data",
-			"width": 120
+			"fieldtype": "Percent",
+			"width": 160
 		},
 	]
 
 def construct_query(filters=None):
 	conditions, filters = get_conditions(filters)
+	month_lower = filters.get("month").lower()
+
 	query = ("""
 			select 
-				rt.cost_center as cost_center,rta.account as account,rta.account_number as account_number,rta.target_amount as target_amount,rta.adjustment_amount as adjustment_amount,rta.net_target_amount as net_target_amount,rta.january as january,rta.february as february,rta.march as march,rta.april as april,rta.may as may,rta.june as june,rta.july as july,rta.august as august,rta.september as september,rta.october as october,rta.november as november,rta.december as december
-			from `tabRevenue Target` rt 
-			inner join `tabRevenue Target Account` rta on rta.parent = rt.name 
-			where rt.docstatus = 1
+				rt.cost_center as cost_center, rta.account as account, 
+		  		rta.account_number as account_number,
+		  		rta.{month} as monthly_amt
+			from `tabRevenue Target` rt, `tabRevenue Target Account` rta 
+			where rta.parent = rt.name and rt.docstatus = 1 
 			{conditions}
-			""".format(conditions=conditions))
-	return query	
+			""".format(month=month_lower, conditions=conditions))
+	return query
+	
 def get_data(query, filters):
 	data = []
 	datas = frappe.db.sql(query, as_dict=True)
+	month_name = filters.get("month")
+	year = filters.get("fiscal_year")
+	month_number = list(calendar.month_name).index(month_name.capitalize())
+	_, end_day = calendar.monthrange(int(year), month_number)
+
+	start_date = f"{year}-{month_number:02d}-01"
+	end_date = f"{year}-{month_number:02d}-{end_day}"
+
 	for d in datas:
-		from_date = filters.get('from_date')
-		to_date = filters.get('to_date')
-		achevied = frappe.db.sql("""
+		achieved = frappe.db.sql("""
 			select
 				ifnull(sum(gl.debit) - sum(gl.credit), 0) as achieved_amount
 				from `tabGL Entry` as gl
@@ -95,111 +96,32 @@ def get_data(query, filters):
 				and gl.posting_date between '{from_date}' and '{to_date}'
 				and gl.cost_center = '{cost_center}'
 				and gl.account ='{account}'
-			""".format(from_date=from_date, to_date=to_date, cost_center=d.cost_center, account=d.account),as_dict=True)
+			""".format(from_date=start_date, to_date=end_date, cost_center=d.cost_center, account=d.account),as_dict=True)
 	
-		achieved_amount = achevied[0]['achieved_amount']
+		achieved_amount = achieved[0]['achieved_amount']
 
-		if not filters.monthly or not filters.month:
-			
-			achieved_percent = 0
-			if achieved_amount > 0:
-				achieved_percent =(flt(achieved_amount)/flt(d.target_amount))*100
-			balance_amount = flt(d.target_amount)-flt(achieved_amount)
-			row = {
-				"cost_center": d.cost_center,
-				"account": d.account,
-				"account_number": d.account_number,
-				"initial_target": d.target_amount,
-				"adestment": d.adestment_amount,
-				"final_target": d.net_target_amount,
-				"achieved_amount":achieved_amount,
-				"balance_amount":balance_amount,
-				"achieved_percent":achieved_percent
-			}
-		else:
-			achieved_percent = 0
-			month = filters.get('month').lower()
-			if month =="january":
-				month_value =d.january
-				balance_amount = flt(d.january)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.january))*100
-			elif month =="february":
-				month_value =d.february
-				balance_amount = flt(d.february)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.february))*100
-			elif month =="march":
-				month_value =d.march
-				balance_amount = flt(d.march)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.march))*100
-			elif month =="april":
-				month_value =d.april
-				balance_amount = flt(d.april)-flt(achieved_amount)
-				if achieved_amount > 0.0:
-					achieved_percent =(flt(achieved_amount)/flt(d.april))*100
-			elif month =="may":
-				month_value =d.may
-				balance_amount =flt(d.may)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.may))*100
-			elif month =="june":
-				month_value =d.june
-				balance_amount = flt(d.june)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.june))*100
-			elif month =="july":
-				month_value =d.july
-				balance_amount = flt(d.july)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.july))*100
-			elif month =="august":
-				month_value =d.august
-				balance_amount = flt(d.august)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.august))*100
-			elif month =="september":
-				month_value =d.september
-				balance_amount = flt(d.september)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.september))*100
-			elif month =="october":
-				month_value =d.october
-				balance_amount =flt(d.october)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.october))*100
-			elif month =="november":
-				month_value =d.november
-				balance_amount = flt(d.november)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.november))*100
-			else:
-				month_value =d.december
-				balance_amount = flt(d.december)-flt(achieved_amount)
-				if achieved_amount > 0:
-					achieved_percent =(flt(achieved_amount)/flt(d.december))*100
-			row = {
-				"cost_center": d.cost_center,
-				"account": d.account,
-				"account_number": d.account_number,
-				"initial_target": month_value,
-				"adestment": d.adestment_amount,
-				"final_target": month_value,
-				"achieved_amount":achieved_amount,
-				"balance_amount":balance_amount,
-				"achieved_percent":achieved_percent
-			}
+		achieved_percent = 0
+		balance_amount = flt(d.monthly_amt) - flt(abs(achieved_amount))
+		if d.monthly_amt != 0:
+			achieved_percent = (flt(achieved_amount) / flt(d.monthly_amt)) * 100
+		row = {
+			"cost_center": d.cost_center,
+			"account": d.account,
+			"account_number": d.account_number,
+			"target_amount": d.monthly_amt,
+			"achieved_amount": abs(achieved_amount),
+			"balance_amount": balance_amount,
+			"achieved_percent": abs(achieved_percent)
+		}
 		data.append(row)
 	return data
+
 def get_conditions(filters):
 	conditions = ""
 	if filters.get("cost_center"):
-		cost_center = filters.get("cost_center")
-		conditions += """and rt.cost_center ='{cost_center}'""".format(cost_center=cost_center)
-	if filters.get("year"):
-		year = filters.get("year")
-		conditions += """and rt.fiscal_year = {year}""".format(year=year)
+		conditions += """ and rt.cost_center ='{cost_center}'""".format(cost_center=filters.get("cost_center"))
+	if filters.get("fiscal_year"):
+		conditions += """ and rt.fiscal_year = '{year}'""".format(year=filters.get("fiscal_year"))
 
 	return conditions, filters
 
