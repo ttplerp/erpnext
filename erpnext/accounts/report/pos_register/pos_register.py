86a1e8a9946692c0a7d6d8b613f42575bb303c9c
jai <jai>
2024-05-17 00:48:28 +0600

    pos reg py added jrnl no column

--- erpnext/accounts/report/pos_register/pos_register.py
+++ erpnext/accounts/report/pos_register/pos_register.py
@@ -63,7 +63,7 @@ def get_pos_entries(filters, group_by_field):
 		SELECT
 			p.posting_date, p.name as pos_invoice, p.pos_profile,
 			p.owner, p.base_grand_total as grand_total, p.base_paid_amount - p.change_amount  as paid_amount,
-			p.customer, p.is_return {select_mop_field}
+			p.customer, p.is_return, p.jrnal_no {select_mop_field}
 		FROM
 			`tabPOS Invoice` p {from_sales_invoice_payment}
 		WHERE
@@ -222,6 +222,12 @@ def get_columns(filters):
 			"fieldname": "mode_of_payment",
 			"fieldtype": "Data",
 			"width": 150,
+		},
+		{
+			"label": _("Reference No."),
+			"fieldname": "jrnal_no",
+			"fieldtype": "Data",
+			"width": 100,
 		},
 		{"label": _("Is Return"), "fieldname": "is_return", "fieldtype": "Data", "width": 80},
 	]
