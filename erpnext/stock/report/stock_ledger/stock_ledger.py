a83a7f119c0ea434d94137d795a9ca81f7c590d2
kinleydorji <kd77144@gmail.com>
2024-07-01 11:57:03 +0600

    stock ledger repolrt

--- erpnext/stock/report/stock_ledger/stock_ledger.py
+++ erpnext/stock/report/stock_ledger/stock_ledger.py
@@ -44,6 +44,8 @@ def execute(filters=None):
 			sle.update(item_detail)
 		if sle.voucher_type == "Stock Entry":
 			sle.expense_account = frappe.db.get_value("Stock Entry Detail", sle.voucher_detail_no, "expense_account")
+		if sle.voucher_type == "Purchase Receipt":
+			sle.expense_account = frappe.db.get_value("Purchase Receipt Item", sle.voucher_detail_no, "expense_account")
 		if filters.get("batch_no") or inventory_dimension_filters_applied:
 			actual_qty += flt(sle.actual_qty, precision)
 			stock_value += sle.stock_value_difference
