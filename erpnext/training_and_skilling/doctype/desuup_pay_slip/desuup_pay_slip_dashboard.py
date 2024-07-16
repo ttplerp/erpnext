b9ad50dbea954ef09bc9d6f044b1c5ec96fb695c
Nyuethyue <dawanyuethyuetshering111@gmail.com>
2024-07-16 17:02:51 +0600

    deleted desuup payment entry

--- erpnext/training_and_skilling/doctype/desuup_pay_slip/desuup_pay_slip_dashboard.py
+++
@@ -1,9 +0,0 @@
-def get_data():
-	return {
-		"fieldname": "journal_entry",
-		"non_standard_fieldnames": {
-			"Journal Entry": "reference_name",
-			"Payment Entry": "reference_name",
-		},
-		"transactions": [{"items": ["Journal Entry"]}],
-	}
--- erpnext/training_and_skilling/doctype/desuup_payroll_entry/desuup_payroll_entry_dashboard.py
+++
@@ -1,9 +0,0 @@
-def get_data():
-	return {
-		"fieldname": "desuup_payroll_entry",
-		"non_standard_fieldnames": {
-			"Journal Entry": "reference_name",
-			"Payment Entry": "reference_name",
-		},
-		"transactions": [{"items": ["Desuup Pay Slip", "Journal Entry"]}],
-	}
