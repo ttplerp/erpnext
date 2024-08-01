7b5faddcda0247010fe853115ca6b1a38a7828ca
Nyuethyue <dawanyuethyuetshering111@gmail.com>
2024-08-01 15:03:20 +0600

    repair and ser

--- erpnext/buying/doctype/purchase_order/purchase_order_dashboard.py
+++ erpnext/buying/doctype/purchase_order/purchase_order_dashboard.py
@@ -1,10 +1,10 @@
 from frappe import _
 
-
 def get_data():
 	return {
 		"fieldname": "purchase_order",
 		"non_standard_fieldnames": {
+			"Advance": "reference_name",
 			"Journal Entry": "reference_name",
 			"Payment Entry": "reference_name",
 			"Payment Request": "reference_name",
@@ -16,12 +16,9 @@ def get_data():
 			"Project": ["items", "project"],
 		},
 		"transactions": [
-			{"label": _("Related"), "items": ["Purchase Receipt", "Purchase Invoice"]},
+			{"label": _("Related"), "items": ["Purchase Receipt", "Purchase Invoice", "Advance"]},
 			{"label": _("Payment"), "items": ["Payment Entry", "Journal Entry", "Payment Request"]},
-			{
-				"label": _("Reference"),
-				"items": ["Material Request", "Supplier Quotation", "Project", "Auto Repeat"],
-			},
+			{"label": _("Reference"), "items": ["Material Request", "Supplier Quotation", "Project", "Auto Repeat"]},
 			{"label": _("Sub-contracting"), "items": ["Subcontracting Order", "Stock Entry"]},
 		],
 	}
