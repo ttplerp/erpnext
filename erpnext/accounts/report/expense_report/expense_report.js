a2df4207cbc32290c38dd96f40ab2f8e8ac256ea
Tshering-Wangchuk <tsheringwangchuky@gmail.com>
2024-11-16 15:18:32 +0600

    export report

--- erpnext/accounts/report/expense_report/expense_report.js
+++ erpnext/accounts/report/expense_report/expense_report.js
@@ -4,6 +4,29 @@
 
 frappe.query_reports["Expense Report"] = {
 	"filters": [
+		{
+			fieldname:"cost_center",
+			label: __("Cost Center"),
+			fieldtype: "Link",
+			options:"Cost Center",
+			
+			
+		},
+		{
+			fieldname:"fiscal_year",
+			label: __("Fiscal Year"),
+			fieldtype: "Link",
+			options:"Fiscal Year",
+			default:"2024"
+			
+		},
+		{
+            "fieldname": "monthly",
+            "label": __("Monthly"),
+            "fieldtype": "Select",
+            "options": "\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12",
+            "default": " "
+        },
 
 	]
 };
--- erpnext/accounts/report/expense_report/expense_report.py
+++ erpnext/accounts/report/expense_report/expense_report.py
@@ -10,37 +10,63 @@ def execute(filters=None):
 
 def get_columns(filters):
 	return [
-			{
+     {
 				"fieldname":"cost_center",
-				"label":"cost_center",
+				"label":"Cost Center",
 				"fieldtype":"data",
 				"options":"",
 				"width":160
 			},
-   {
+      {
 				"fieldname":"total_expense",
-				"label":"total_expense",
+				"label":"Total Expense",
 				"fieldtype":"data",
 				"options":"",
 				"width":160
 			},
-   {
+     {
 				"fieldname":"total_income",
-				"label":"total_income",
+				"label":"Income",
 				"fieldtype":"data",
 				"options":"",
 				"width":160
 			},
-    {
+     {
+				"fieldname":"total_direct_expense",
+				"label":"Direct Expense",
+				"fieldtype":"data",
+				"options":"",
+				"width":160
+			},
+      {
+				"fieldname":"total_indirect_expense",
+				"label":"Indirect Expense",
+				"fieldtype":"data",
+				"options":"",
+				"width":160
+			},
+			{
+				"fieldname":"gross_profit",
+				"label":"Gross Profit",
+				"fieldtype":"data",
+				"options":"",
+				"width":160
+			},
+  
+   
+	{
 				"fieldname":"profit",
-				"label":"profit",
+				"label":"Net Profit",
 				"fieldtype":"data",
 				"options":"",
 				"width":160
 			},
+ 
+ 
 		]
 
 def get_data(filters):
+	conditions = get_conditions(filters)
 	data = []
 	# data = frappe.db.sql('''
 	# 					SELECT gl.cost_center, 
@@ -52,22 +78,63 @@ def get_data(filters):
 	#   gl.account = a.name where 
 	#   a.account_type="Expense Account" and gl.company="VAJRA BUILDERS PRIVATE LIMITED"  group by gl.cost_center''',as_dict=1)
 	
-	data = frappe.db.sql('''
-						SELECT
-	gl.cost_center,
-	SUM(CASE WHEN a.root_type = "Income" THEN gl.credit - gl.debit ELSE 0 END) AS total_income,
-	SUM(CASE WHEN a.root_type in ("Expense", "Depreciation","Stock Adjustment") THEN gl.debit - gl.credit ELSE 0 END) AS total_expense,
-	SUM(CASE WHEN a.account_type = "Income Account" THEN gl.credit - gl.debit ELSE 0 END) -
-	SUM(CASE WHEN a.account_type = "Expense Account" THEN gl.debit - gl.credit ELSE 0 END) AS profit
-FROM
-	`tabGL Entry` AS gl
-INNER JOIN
-	`tabAccount` AS a
-ON
-	gl.account = a.name
-WHERE
-	gl.company = "VAJRA BUILDERS PRIVATE LIMITED" and fiscal_year="2024"
-GROUP BY
-	gl.cost_center''',as_dict=1)
+	data = frappe.db.sql(
+	 			'''
+				SELECT
+					gl.cost_center,
+					SUM(CASE WHEN a.root_type = "Income" THEN gl.credit - gl.debit ELSE 0 END) AS total_income,
+					SUM(CASE WHEN a.root_type IN ("Expense", "Depreciation", "Stock Adjustment") THEN gl.debit - gl.credit ELSE 0 END) AS total_expense,
+					SUM(CASE WHEN a.root_type = "Income" THEN gl.credit - gl.debit ELSE 0 END) -
+					SUM(CASE WHEN a.root_type IN ("Expense", "Depreciation", "Stock Adjustment") THEN gl.debit - gl.credit ELSE 0 END) AS profit,
+					SUM(
+						CASE 
+							WHEN (a2.parent_account = "51 - Direct Expenses" or a3.parent_account = "51 - Direct Expenses")
+							THEN gl.debit - gl.credit 
+							ELSE 0 
+						END
+					) AS total_direct_expense,
+					SUM(
+						CASE 
+							WHEN (a2.parent_account = "60 - Indirect Expense" or a3.parent_account = "60 - Indirect Expense")
+							THEN gl.debit - gl.credit 
+							ELSE 0 
+						END
+					) AS total_indirect_expense,
+					SUM(CASE WHEN a.root_type = "Income" THEN gl.credit - gl.debit ELSE 0 END) - 
+					SUM(
+						CASE 
+							WHEN (a2.parent_account = "51 - Direct Expenses" or a3.parent_account = "51 - Direct Expenses") 
+							THEN gl.debit - gl.credit 
+							ELSE 0 
+						END
+					) AS gross_profit
+				FROM
+					`tabGL Entry` AS gl
+				INNER JOIN
+					`tabAccount` AS a ON gl.account = a.name
+				INNER JOIN
+					`tabAccount` AS a2 ON a.parent_account = a2.name
+				INNER JOIN
+					`tabAccount` AS a3 ON a2.parent_account = a3.name
+				WHERE
+					gl.company = "VAJRA BUILDERS PRIVATE LIMITED" 
+					{conditions}
+				GROUP BY 
+					gl.cost_center;
+
+ 		'''.format(conditions=conditions),as_dict=1)
 	
 	return data
+
+
+def get_conditions(filters):
+	conditions = []
+	if filters and filters.get("fiscal_year"):
+		conditions.append("fiscal_year = '{}'".format(filters.get("fiscal_year")))
+	if filters.get("monthly"):
+		conditions.append("MONTH(gl.posting_date) = '{}'".format(filters.get("monthly")))
+	if filters and filters.get("cost_center"):
+		conditions.append("gl.cost_center = '{}'".format(filters.get("cost_center")))
+
+	return "AND {}".format(" AND ".join(conditions)) if conditions else ""
+
