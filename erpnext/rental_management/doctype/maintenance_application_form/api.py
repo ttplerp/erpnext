0a32b27eff1550632cfb6096971d7f2ab3ac25af
jai <jai>
2024-12-05 20:53:12 +0600

    api works

--- erpnext/rental_management/doctype/maintenance_application_form/api.py
+++ erpnext/rental_management/doctype/maintenance_application_form/api.py
@@ -3,6 +3,7 @@ import json
 from frappe.model.document import Document
 from werkzeug.wrappers import Response
 from frappe.utils import response as r
+from frappe.utils import nowdate,date_diff
 
 @frappe.whitelist()
 def get_all_maf():
@@ -10,7 +11,8 @@ def get_all_maf():
 
 	return datas
 
-@frappe.whitelist(methods=['POST'])
+# @frappe.whitelist(methods=['POST'])
+@frappe.whitelist(allow_guest=True)
 def create_maf():
 	data = json.loads(frappe.request.data)
 	tenant_info = get_cid_detail(data.get('cid'))
@@ -112,6 +114,44 @@ def tenant_cid(tenant_cid):
 			return {'status_code':200,  'message': 'Successful', 'data': data}
 		else:
 			return {'status_code':404,  'message': 'No dataaaa found!', 'data': {}}
+	except Exception as e:
+		frappe.log_error(_("Error in Tenant Info.: {0}").format(e))
+		return {'status_code':502,  'message': 'Server error'}
+
+# @frappe.whitelist(methods=['POST'])
+@frappe.whitelist(allow_guest=True)
+def housing_clearance():
+	data = json.loads(frappe.request.data)
+
+	try:
+		""" check existing """
+		for a in frappe.db.sql("""
+							select name, application_status, application_date, docstatus
+							from `tabHousing Clearance`
+							where cid='{}'
+							and docstatus != 2
+							""".format(data.get('cid')), as_dict=True):
+			if a.application_status == "Pending":
+				msg = f'Your Housing Clearance Application {a.name} is still Pending'
+				return {'status_code':200,  'message': msg}
+
+			if a.application_status == "Approved" and a.docstatus==1:
+				num_day = date_diff(nowdate(), a.application_date)
+				if num_day < 90:
+					msg = f'Your Housing Clearance Application {a.name} is Not Expired'
+					return {'status_code':200,  'message': msg}
+		
+		doc = frappe.new_doc('Housing Clearance')
+		# doc.ordered_by = frappe.session.user
+		doc.applicant_type = data.get('applicant_type')
+		doc.cid = data.get('cid')
+		doc.application_name = data.get('applicant_name')
+		doc.purpose = data.get('purpose')
+		doc.email = data.get('email')
+		doc.phone_no = data.get('phone_no')
+		doc.insert()
+
+		return {'status_code':200,  'message': 'Housing Clearance Form Successfully Applied'}
 	except Exception as e:
 		frappe.log_error(_("Error in Tenant Info.: {0}").format(e))
 		return {'status_code':502,  'message': 'Server error'}
