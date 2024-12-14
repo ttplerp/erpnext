a64f87b87554b1cc4761f88493c358747ac06a3c
thukten <thukday@gmail.com>
2024-12-14 23:57:29 +0600

    PMS Changes by Thukten

--- erpnext/pms/doctype/target_set_up/target_set_up.js
+++ erpnext/pms/doctype/target_set_up/target_set_up.js
@@ -135,6 +135,14 @@ var add_btn = (frm)=>{
 			});
 		}).addClass("btn-primary custom-create custom-create-css");
 	}
+	if (frm.doc.docstatus == 1){
+		frm.add_custom_button(__('Create Evaluation'), ()=>{
+			frappe.model.open_mapped_doc({
+				method: "erpnext.pms.doctype.target_set_up.target_set_up.create_evaluation",	
+				frm: cur_frm
+			});
+		}).addClass("btn-primary custom-create custom-create-css")
+	}
 }
 
 var hr_add_btn = (frm)=>{
