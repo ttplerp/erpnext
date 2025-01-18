bfdb5683ebe38218081678489cd0d4cf858d5e66
Nyuethyue <dawa>
2025-01-18 16:16:53 +0600

    tds register

--- erpnext/accounts/report/tds_register/tds_register.js
+++ erpnext/accounts/report/tds_register/tds_register.js
@@ -5,16 +5,37 @@
 frappe.query_reports["TDS Register"] = {
 	"filters": [
 		{
+			"fieldname": "fiscal_year",
+			"label": __("Fiscal Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": frappe.defaults.get_user_default("fiscal_year"),
+			"reqd": 1,
+			"on_change": function(query_report) {
+				var fiscal_year = query_report.get_values().fiscal_year;
+				if (!fiscal_year) {
+					return;
+				}
+				frappe.model.with_doc("Fiscal Year", fiscal_year, function(r) {
+					var fy = frappe.model.get_doc("Fiscal Year", fiscal_year);
+					frappe.query_report.set_filter_value({
+						from_date: fy.year_start_date,
+						to_date: fy.year_end_date
+					});
+				});
+			}
+		},
+		{
 			"fieldname": "from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date",
-			"reqd": 1
+			"default": frappe.defaults.get_user_default("year_start_date"),
 		},
 		{
 			"fieldname": "to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
-			"reqd": 1
+			"default": frappe.defaults.get_user_default("year_end_date"),
 		},
 		{
 			"fieldname": "tax_withholding_category",
