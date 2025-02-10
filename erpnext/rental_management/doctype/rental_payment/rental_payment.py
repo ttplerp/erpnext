5cf2bdbe03d83436022f8312538e52d3e9fb24c6
jai <jai>
2025-02-10 11:13:13 +0600

    rental payment update

--- erpnext/rental_management/doctype/rental_payment/rental_payment.py
+++ erpnext/rental_management/doctype/rental_payment/rental_payment.py
@@ -129,7 +129,7 @@ class RentalPayment(AccountsController):
 					a.balance_rent = flt(a.bill_amount) - flt(a.rent_received) - flt(a.tds_amount) - flt(a.discount_amount)
 					
 					frappe.msgprint("Rent Received amount is changed to {} as the total of Rent receive + Discount + TDS cannot be more than Bill Amount {} for tenant {}".format(a.rent_received, a.bill_amount, a.tenant_name))
-			a.total_amount_received = flt(a.rent_received) + flt(a.security_deposit_amount) + flt(a.penalty) + flt(a.excess_amount) + flt(a.pre_rent_amount_received)
+			a.total_amount_received = round(a.rent_received) + round(a.security_deposit_amount) + round(a.penalty) + round(a.excess_amount) + round(a.pre_rent_amount_received)
 			# frappe.throw("not here! check on balance rent")
 			""" commented below code to enable opening accumulated rent receive in chunks with excess amount. for time being. """
 			# if a.balance_rent > 0 and (a.pre_rent_amount > 0 or a.excess_amount > 0):
@@ -144,7 +144,7 @@ class RentalPayment(AccountsController):
 			excess += flt(a.excess_amount)
 			pre_rent += flt(a.pre_rent_amount)
 			# property_mgt_amount += flt(a.property_management_amount)
-			total_amount_received += flt(a.rent_received, 2) + flt(a.security_deposit_amount, 2) + flt(a.penalty, 2) + flt(a.excess_amount, 2) + flt(a.pre_rent_amount_received, 2)
+			total_amount_received += round(a.rent_received) + round(a.security_deposit_amount) + round(a.penalty) + round(a.excess_amount) + round(a.pre_rent_amount_received)
 
 		if self.rent_write_off:
 			self.rent_write_off_amount = write_off_amount
@@ -155,7 +155,7 @@ class RentalPayment(AccountsController):
 		self.pre_rent_amount = pre_rent
 		self.tds_amount = tds_amount
 		self.property_management_amount = property_mgt_amount
-		self.total_amount_received = flt(total_amount_received, 2)
+		self.total_amount_received = round(total_amount_received)
 		# if self.tds_amount > 0 and not self.tds_account:
 		# 	frappe.throw("Please select TDS Account")
 
