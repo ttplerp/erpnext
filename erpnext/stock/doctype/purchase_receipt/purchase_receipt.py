4236d6cbd86abb46efcfa80fc9543d5abc6df702
jai <jai>
2025-11-06 12:00:58 +0600

    auditor role permission in PI,PO,PO

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
@@ -1146,7 +1146,7 @@ def get_permission_query_conditions(user):
 	if not user: user = frappe.session.user
 	user_roles = frappe.get_roles(user)
 
-	if user == "Administrator" or "System Manager" in user_roles or "Stock Master" in user_roles: 
+	if user == "Administrator" or "System Manager" in user_roles or "Stock Master" in user_roles or "Auditor" in user_roles: 
 		return
 
 	return """(
