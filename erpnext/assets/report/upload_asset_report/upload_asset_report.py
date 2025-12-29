12603a8bbb2b9dec43f9cabfedf1b1aa9f3bff94
jai <jai>
2025-12-29 17:55:53 +0600

    upload asset report update

--- erpnext/assets/report/upload_asset_report/upload_asset_report.py
+++ erpnext/assets/report/upload_asset_report/upload_asset_report.py
@@ -48,7 +48,8 @@ def get_data(filters):
 						{cond}
 						and voucher_no not in (
 							select journal_entry_for_scrap from tabAsset where disposal_date between "{from_date}" and "{to_date}"
-						) 
+						)
+						and gl.voucher_no not in ( select name from `tabJournal Entry` where docstatus=1 and reverse_jv = 1 ) 
 						group by account, cost_center 
 						order by cost_center
 					""".format(accounts=accounts, from_date=from_date, to_date=to_date, cond=cond), as_dict=True):
