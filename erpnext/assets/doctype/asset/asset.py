32a380aa05a9eeea813561f28368e033bb206ff1
jairaj <jairajrai1@gmail.com>
2026-05-28 17:43:36 +0600

    asset change

--- erpnext/assets/doctype/asset/asset.py
+++ erpnext/assets/doctype/asset/asset.py
@@ -1293,7 +1293,7 @@ def get_permission_query_conditions(user):
 	if not user: user = frappe.session.user
 	user_roles = frappe.get_roles(user)
 
-	if user == "Administrator" or "System Manager" in user_roles: 
+	if user == "Administrator" or "System Manager" or "Asset Manager" in user_roles: 
 		return
 
 	return """(
