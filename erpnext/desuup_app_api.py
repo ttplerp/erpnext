26af9579a15e31eb03581ab3d3277b1698b9101f
kxt5258 <kxt5258@gmail.com>
2026-06-03 17:28:17 +0600

    permission for attendance submission

--- erpnext/desuup_app_api.py
+++ erpnext/desuup_app_api.py
@@ -177,7 +177,7 @@ def submit_attendance(deployment, date, attendance_detail):
             att.attendance_date = date
             att.reference_doctype = "Desuup Deployment"
             att.reference_name = deployment
-            att.submit()
+            att.submit(ignore_permissions=True)
         else:
             att = frappe.get_doc("Desuup Attendance", n)
             att.db_set("status", s)
