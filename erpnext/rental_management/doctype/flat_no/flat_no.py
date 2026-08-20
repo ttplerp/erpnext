968b50c1a34886a250374935e470a1d5249fbfc2
Lhendup97 <k.lhendup97@gmail.com>
2026-08-20 12:13:15 +0600

    sync dev to prod

--- erpnext/rental_management/doctype/flat_no/flat_no.py
+++ erpnext/rental_management/doctype/flat_no/flat_no.py
@@ -16,6 +16,7 @@ class FlatNo(Document):
 		# self.validate_luc_duplicate()
 		self.validate_duplicate_item_entry()
 		self.calc_total_prop_amount()
+		self.update_building_classification()
 
 	def calc_total_prop_amount(self):
 		prop_mgt_amt = 0
@@ -35,3 +36,16 @@ class FlatNo(Document):
 				data.append(d.property_management_type)
 			else:
 				frappe.throw("Duplicate data entry at #Row. {}".format(d.idx))
+
+	def update_building_classification(self):
+		building_class_result = frappe.db.sql("""
+			SELECT name 
+			FROM `tabBuilding Classification`
+			WHERE %s BETWEEN minimum_floor_area AND maximum_floor_area
+		""", (self.total_floor_area))
+		
+		if not building_class_result or not building_class_result[0]:
+			frappe.throw("Building Classification not found")
+		
+		building_class = building_class_result[0][0]
+		self.building_classification = building_class
